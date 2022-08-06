3a4d52809355f5c94937003ecd9ff4d42a95f683
avoytenko-rewind <88456133+avoytenko-rewind@users.noreply.github.com>
2022-08-05 23:28:03 -0400

    initial

---
+++ rewind-gutenblocks.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name:       Rewind Gutenblocks
+ * Description:       Example static block scaffolded with Create Block tool.
+ * Requires at least: 5.9
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       rewind-gutenblocks
+ *
+ * @package           create-block
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_rewind_blocks() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'create_rewind_blocks' );
